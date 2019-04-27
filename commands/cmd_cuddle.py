b23283183b4380e83f57c70c552795b44c73317c
TheRealCed <39238694+TheRealCed@users.noreply.github.com>
2019-04-27 02:22:11 +0200

    Update cmd_cuddle.py

--- commands/cmd_cuddle.py
+++ commands/cmd_cuddle.py
@@ -6,4 +6,4 @@ import time
 import random
 
 async def ex(args, message, client, invoke):
-	await client.send_message(message.channel, random.choice(["Stop..", "T-That.. feels good...", "Why do you.. do this?", "Umm.. Why don't y-you cuddle C-Cocoa-san?", "I-I'm sorry.. I don't want that right n-now....", "Oh.. T-That surprised me...", "W-Why me..? I'm not much...", "I liked that.. Can you p-please do more??", "*blushes* I love it when y-you c-cuddle me..", "J-Just... no... sorry."]))
+	await client.send_message(message.channel, "test")
